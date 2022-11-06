b8067a42eff36bc82192d0ea0f110f9691b5b2b9
Anastasia Lovtsova <lovcova.anastasi@mail.ru>
2022-11-06 19:34:52 +0400

    Tblue-1.2-top_panel:added verification of an authorized user

--- conscious-citizen/src/app/components/main/top-panel/top-panel.component.ts
+++ conscious-citizen/src/app/components/main/top-panel/top-panel.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {TokenService} from "../../../services/token.service";
 
 @Component({
     selector: 'app-top-panel',
@@ -7,11 +8,10 @@ import {Router} from "@angular/router";
 })
 export class TopPanelComponent implements OnInit {
 
-    constructor(private router: Router) {
+    constructor(private router: Router, public tokenService: TokenService) {
     }
 
     ngOnInit(): void {
-
     }
 
     navigateToProfile() {
